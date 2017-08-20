f7ccf828df2e4cfa92016be5238ff671d32ad1c1
HenryCo <hd2files@gmail.com>
2017-08-21 00:48:55 +0200

    Initial commit

---
+++ src/main/java/net/henryco/blinckserver/BlinckServerApplication.java
@@ -0,0 +1,12 @@
+package net.henryco.blinckserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinckServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinckServerApplication.class, args);
+	}
+}
