0ab008c03a9b1de803daf2a151296c597cf3b0ba
HenryCo <hd2files@gmail.com>
2017-08-27 03:27:20 +0200

    Junit tests added

---
+++ src/test/java/net/henryco/blinckserver/unit/auth/TokenAuthTest.java
@@ -0,0 +1,18 @@
+package net.henryco.blinckserver.unit.auth;
+
+import net.henryco.blinckserver.unit.BlinckUnitTest;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Henry on 27/08/17.
+ */
+public class TokenAuthTest extends BlinckUnitTest {
+
+	@Test
+	public void test() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		// TODO: 27/08/17
+	}
+
+}
