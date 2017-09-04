6e7c33e3ca0d6485a4783346f6a0645c31414c31
HenryCo <henrycodev@gmail.com>
2017-09-04 16:47:34 +0200

    Friendship queue work

--- src/main/java/net/henryco/blinckserver/mvc/service/relation/core/FriendshipService.java
+++ src/main/java/net/henryco/blinckserver/mvc/service/relation/core/FriendshipService.java
@@ -2,6 +2,7 @@ package net.henryco.blinckserver.mvc.service.relation.core;
 
 import net.henryco.blinckserver.mvc.model.dao.relation.core.FriendshipDao;
 import net.henryco.blinckserver.mvc.model.entity.relation.core.Friendship;
+import net.henryco.blinckserver.mvc.model.entity.relation.queue.FriendshipNotification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,15 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Date;
 import java.util.List;
 
-
 /**
  * @author Henry on 03/09/17.
- */
-@Service
+ */ @Service
 public class FriendshipService {
 
 	private final FriendshipDao friendshipDao;
-
 
 	@Autowired
 	public FriendshipService(FriendshipDao friendshipDao) {
@@ -25,18 +23,15 @@ public class FriendshipService {
 	}
 
 
-
-
 	/**
 	 * Create and save new friendship relation between users.
 	 * @return <b>ID</b> of saved relation id database.
-	 */
-	@Transactional
-	public Long saveFriendshipRelation(Long user1, Long user2) {
+	 */ @Transactional
+	public Long saveFriendshipRelation(FriendshipNotification notification) {
 
 		Friendship friendship = new Friendship();
-		friendship.setUser1(user1);
-		friendship.setUser2(user2);
+		friendship.setUser1(notification.getInitiatorId());
+		friendship.setUser2(notification.getReceiverId());
 		friendship.setDate(new Date(System.currentTimeMillis()));
 		return friendshipDao.save(friendship).getId();
 	}
@@ -50,8 +45,7 @@ public class FriendshipService {
 
 	/**
 	 * Delete <b>ALL</b> friendship relations with user.
-	 */
-	@Transactional
+	 */ @Transactional
 	public void deleteAllUserRelations(Long user) {
 		friendshipDao.deleteAllByUserId(user);
 	}
@@ -60,11 +54,27 @@ public class FriendshipService {
 	/**
 	 * Delete friendship relation between users.<br>
 	 * <b>Arguments order doesn't matter.</b>
-	 */
-	@Transactional
+	 */ @Transactional
 	public void deleteRelationBetweenUsers(Long user1, Long user2) {
 		friendshipDao.deleteRelationBetweenUsers(user1, user2);
 	}
 
+
+	@Transactional
+	public Friendship getById(Long id) {
+		return friendshipDao.getById(id);
+	}
+
+
+	@Transactional
+	public boolean isExistsById(Long id) {
+	 	return friendshipDao.isExists(id);
+	}
+
+
+	@Transactional
+	public void deleteById(Long id) {
+	 	friendshipDao.deleteById(id);
+	}
 
 }
