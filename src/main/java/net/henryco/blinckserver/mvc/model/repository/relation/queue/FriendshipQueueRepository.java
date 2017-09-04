9d095c7d75c1e0ff60be841a062337984720b6f0
HenryCo <henrycodev@gmail.com>
2017-09-04 02:01:16 +0200

    Friendship queue work

--- src/main/java/net/henryco/blinckserver/mvc/model/repository/relation/queue/FriendshipQueueRepository.java
+++ src/main/java/net/henryco/blinckserver/mvc/model/repository/relation/queue/FriendshipQueueRepository.java
@@ -1,12 +1,23 @@
 package net.henryco.blinckserver.mvc.model.repository.relation.queue;
 
 import net.henryco.blinckserver.mvc.model.entity.relation.queue.FriendshipQueue;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * @author Henry on 04/09/17.
  */
 public interface FriendshipQueueRepository extends JpaRepository<FriendshipQueue, Long> {
 
+	List<FriendshipQueue> getAllByInitiatorIdOrReceiverIdOrderByDateDesc(Long initiatorId, Long receiverId, Pageable pageable);
+	List<FriendshipQueue> getAllByInitiatorIdOrderByDateDesc(Long initiatorId, Pageable pageable);
+	List<FriendshipQueue> getAllByReceiverIdOrderByDateDesc(Long receiverId, Pageable pageable);
+
+	void removeAllByReceiverIdAndInitiatorIdOrInitiatorIdAndReceiverId(Long receiverId, Long initiatorId, Long initiatorId2, Long receiverId2);
+	void removeAllByInitiatorIdOrReceiverId(Long initiatorId, Long receiverId);
+	void removeAllByInitiatorId(Long initiatorId);
+	void removeAllByReceiverId(Long receiverId);
 
 }
---
+++ src/main/java/net/henryco/blinckserver/mvc/service/relation/queue/FriendshipQueueService.java
@@ -0,0 +1,23 @@
+package net.henryco.blinckserver.mvc.service.relation.queue;
+
+import net.henryco.blinckserver.mvc.model.dao.relation.queue.FriendshipQueueDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Henry on 04/09/17.
+ */
+@Service
+public class FriendshipQueueService {
+
+	private final FriendshipQueueDao friendshipQueueDao;
+
+	@Autowired
+	public FriendshipQueueService(FriendshipQueueDao friendshipQueueDao) {
+		this.friendshipQueueDao = friendshipQueueDao;
+	}
+
+
+
+
+}
