165b3e2770866472b2b90f31ae765a364435b537
HenryCo <henrycodev@gmail.com>
2017-09-06 19:48:26 +0200

    Friendship CONVERSATION controller finished and TESTED (and passed)

--- src/main/java/net/henryco/blinckserver/mvc/service/infrastructure/UpdateNotificationService.java
+++ src/main/java/net/henryco/blinckserver/mvc/service/infrastructure/UpdateNotificationService.java
@@ -1,10 +1,17 @@
 package net.henryco.blinckserver.mvc.service.infrastructure;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import net.henryco.blinckserver.mvc.model.dao.infrastructure.UpdateNotificationDao;
 import net.henryco.blinckserver.mvc.model.entity.infrastructure.UpdateNotification;
 import net.henryco.blinckserver.util.dao.BlinckDaoProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Henry on 06/09/17.
@@ -12,6 +19,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class UpdateNotificationService
 		extends BlinckDaoProvider<UpdateNotification, Long> {
+
+
+	@Data @NoArgsConstructor @AllArgsConstructor
+	public static final class SimpleNotification {
+
+		private Long targetUserId;
+		private String notification;
+		private String type;
+	}
 
 
 	@Autowired
@@ -23,6 +39,60 @@ public class UpdateNotificationService
 		return provideDao();
 	}
 
+
+	@Transactional
+	public void addNotification(SimpleNotification simpleNotification) {
+		getDao().save(createNotification(simpleNotification));
+	}
+
+	@Transactional
+	public void addNotification(Long targetUserId, String type, String notification) {
+		addNotification(new SimpleNotification(targetUserId, type, notification));
+	}
+
+
+	@Transactional
+	public List<UpdateNotification> getAllUserNotifications(Long userId, int page, int size) {
+		return getDao().getAllNotificationsByUserIdOrderDesc(userId, page, size);
+	}
+
+	@Transactional
+	public List<UpdateNotification> getAllUserNotifications(Long userId) {
+		return getDao().getAllNotificationsByUserIdOrderDesc(userId);
+	}
+
+	@Transactional
+	public long countAllUserNotifications(Long userId) {
+		return getDao().countUserNotifications(userId);
+	}
+
+	@Transactional
+	public void removeAllUserNotifications(Long userId) {
+		getDao().removeUserNotifications(userId);
+	}
+
+
+
+
+	private static UpdateNotification.Details
+	createDetails(SimpleNotification simpleNotification) {
+
+		final UpdateNotification.Details details = new UpdateNotification.Details();
+		details.setType(simpleNotification.getType());
+		details.setNotification(simpleNotification.getNotification());
+		return details;
+	}
+
+
+	private static UpdateNotification
+	createNotification(SimpleNotification simpleNotification) {
+
+		final UpdateNotification notification = new UpdateNotification();
+		notification.setTargetUserId(simpleNotification.getTargetUserId());
+		notification.setDate(new Date(System.currentTimeMillis()));
+		notification.setDetails(createDetails(simpleNotification));
+		return notification;
+	}
 
 
 }
