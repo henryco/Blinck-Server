9dbf108f278b62b2f458bb8395df5eb5f988e73e
HenryCo <henrycodev@gmail.com>
2017-09-06 20:43:37 +0200

    Friendship CONVERSATION controller finished and TESTED (and passed)

--- src/main/java/net/henryco/blinckserver/mvc/controller/secured/user/UserProfileController.java
+++ src/main/java/net/henryco/blinckserver/mvc/controller/secured/user/UserProfileController.java
@@ -3,7 +3,6 @@ package net.henryco.blinckserver.mvc.controller.secured.user;
 import net.henryco.blinckserver.mvc.controller.secured.BlinckProfileController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Henry on 26/08/17.
---
+++ src/main/java/net/henryco/blinckserver/mvc/controller/secured/user/notifications/UserNotificationsController.java
@@ -0,0 +1,58 @@
+package net.henryco.blinckserver.mvc.controller.secured.user.notifications;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.henryco.blinckserver.mvc.controller.BlinckController;
+import net.henryco.blinckserver.mvc.model.entity.infrastructure.UpdateNotification;
+import net.henryco.blinckserver.mvc.service.infrastructure.UpdateNotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *  @author Henry on 06/09/17.
+ */ @RestController
+@RequestMapping("/protected/user/notifications")
+public class UserNotificationsController implements BlinckController {
+
+ 	private final UpdateNotificationService updateNotificationService;
+
+ 	@Autowired
+	public UserNotificationsController(
+			UpdateNotificationService updateNotificationService) {
+		this.updateNotificationService = updateNotificationService;
+	}
+
+
+	@Data @NoArgsConstructor
+	private static final class NotificationForm
+			implements Serializable{
+
+ 		private Long id;
+ 		private Long user;
+ 		private String type;
+ 		private String info;
+		private Date timestamp;
+
+		private NotificationForm(
+				UpdateNotification notification) {
+
+			setId(notification.getId());
+			setUser(notification.getTargetUserId());
+			setType(notification.getDetails().getType());
+			setInfo(notification.getDetails().getNotification());
+			setTimestamp(notification.getDate());
+		}
+
+	}
+
+
+
+
+
+
+}
