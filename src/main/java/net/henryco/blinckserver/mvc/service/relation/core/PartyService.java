6f84be0b97dbec5fad9e4f1be36018ca0c8f55ba
HenryCo <henrycodev@gmail.com>
2017-09-17 21:13:55 +0200

    Matching controllers and service added (cleanup)

--- src/main/java/net/henryco/blinckserver/mvc/service/relation/core/PartyService.java
+++ src/main/java/net/henryco/blinckserver/mvc/service/relation/core/PartyService.java
@@ -1,28 +1,45 @@
 package net.henryco.blinckserver.mvc.service.relation.core;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import net.henryco.blinckserver.mvc.model.dao.relation.core.PartyDao;
 import net.henryco.blinckserver.mvc.model.entity.relation.core.Party;
+import net.henryco.blinckserver.util.dao.BlinckDaoProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
 
 /**
  * @author Henry on 13/09/17.
  */
 @Service
-public class PartyService {
+public class PartyService extends BlinckDaoProvider<Party, Long> {
 
-	private final PartyDao partyDao;
 
-	@Autowired
-	public PartyService(PartyDao partyDao) {
-		this.partyDao = partyDao;
+	@Data @NoArgsConstructor
+	public static final class PartyInfo
+			implements Serializable {
+
+		private PartyInfo(Party party) {
+			// TODO: 17/09/17
+		}
 	}
 
 
+	@Autowired
+	public PartyService(PartyDao partyDao) {
+		super(((partyDao)));
+	}
+
+	private PartyDao getDao() {
+		return provideDao();
+	}
+
 	@Transactional
 	public Party[] getAllParties() {
-		return partyDao.getAll().toArray(new Party[0]);
+		return getDao().getAll().toArray(new Party[0]);
 	}
 
 }
