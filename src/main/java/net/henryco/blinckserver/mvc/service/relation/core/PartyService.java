b4257af7a9603f297ca4a0f388a6569423ed8716
HenryCo <henrycodev@gmail.com>
2017-09-18 16:37:08 +0200

    SubParty and Party conversation controllers, services, dao's etc etc

--- src/main/java/net/henryco/blinckserver/mvc/service/relation/core/PartyService.java
+++ src/main/java/net/henryco/blinckserver/mvc/service/relation/core/PartyService.java
@@ -1,15 +1,20 @@
 package net.henryco.blinckserver.mvc.service.relation.core;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.henryco.blinckserver.mvc.model.dao.relation.core.PartyDao;
 import net.henryco.blinckserver.mvc.model.entity.relation.core.Party;
+import net.henryco.blinckserver.mvc.model.entity.relation.core.embeded.Type;
 import net.henryco.blinckserver.util.dao.BlinckDaoProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Henry on 13/09/17.
@@ -22,8 +27,16 @@ public class PartyService extends BlinckDaoProvider<Party, Long> {
 	public static final class PartyInfo
 			implements Serializable {
 
+		private @JsonProperty Long id;
+		private @JsonProperty Type type;
+		private @JsonProperty("enable_since") Date date;
+		private @JsonProperty("sub_parties") List<Long> subParties;
+
 		private PartyInfo(Party party) {
-			// TODO: 17/09/17
+			this.id = party.getId();
+			this.type = party.getDetails().getType();
+			this.date = party.getActivationTime();
+			this.subParties = new ArrayList<>(party.getSubParties());
 		}
 	}
 
