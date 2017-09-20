674166a6a00b7571d482ffe762fb3cd22ef3cd31
HenryCo <henrycodev@gmail.com>
2017-09-21 00:12:01 +0200

    SubParty and Party conversation controllers, services, dao's etc etc

--- src/main/java/net/henryco/blinckserver/mvc/controller/secured/user/UserProfileController.java
+++ src/main/java/net/henryco/blinckserver/mvc/controller/secured/user/UserProfileController.java
@@ -2,15 +2,31 @@ package net.henryco.blinckserver.mvc.controller.secured.user;
 
 import net.henryco.blinckserver.mvc.controller.BlinckController;
 import net.henryco.blinckserver.mvc.controller.secured.BlinckProfileController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Component
+final class UserProfileServicePack {
+
+	// TODO: 21/09/17
+}
 
 /**
  * @author Henry on 26/08/17.
- */ @Controller
+ */ @RestController
 @RequestMapping(BlinckController.EndpointAPI.USER)
 public class UserProfileController implements BlinckProfileController {
 
+	private final UserProfileServicePack services;
+
+	@Autowired
+	public UserProfileController(UserProfileServicePack services) {
+		this.services = services;
+	}
 
 
 
